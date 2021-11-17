682399dfecf4d02275ec3767422b4f905f62a790
Fabien Chouteau <chouteau@adacore.com>
2021-11-17 12:49:25 +0100

    Test

--- src/gnatcov_poc.adb
+++ src/gnatcov_poc.adb
@@ -15,6 +15,16 @@ procedure Gnatcov_Poc is
          return X * 2;
       end if;
    end B;
+
+   function C(X : Integer) return Integer is
+   begin
+      if (X mod 2) = 1 then
+         return A;
+      else
+         return X * 2;
+      end if;
+   end C;
 begin
    Ada.Text_IO.Put_Line (B (1)'Img);
+   Ada.Text_IO.Put_Line (C (A)'Img);
 end Gnatcov_Poc;
